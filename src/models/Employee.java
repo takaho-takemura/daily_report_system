69a6c3c4d6eb0ec8c14bcc4d4a19527de956f7b4
takaho.takemura <takapon.git@gmail.com>
2019-09-03 16:59:10 +0900

    add login / logout

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -11,25 +11,24 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-
-@Table(name="employees")
+@Table(name = "employees")
 @NamedQueries({
     @NamedQuery(
-            name= "getAllEmployees",
-            query="SELECT e FROM Employee AS e ORDER BY e.id DESC"
+            name = "getAllEmployees",
+            query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
             ),
     @NamedQuery(
-            name="getEmployeesCount",
-            query="SELECT COUNT(e) FROM Employee AS e"
+            name = "getEmployeesCount",
+            query = "SELECT COUNT(e) FROM Employee AS e"
             ),
     @NamedQuery(
-            name="checkRegisteredCode",
-            query="SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
+            name = "checkRegisteredCode",
+            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
             ),
     @NamedQuery(
-            name="checkLoginCodeAndPassword",
-            query="SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
-            ),
+            name = "checkLoginCodeAndPassword",
+            query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+            )
 })
 @Entity
 public class Employee {
@@ -122,6 +121,4 @@ public class Employee {
     public void setDelete_flag(Integer delete_flag) {
         this.delete_flag = delete_flag;
     }
-
-
 }
